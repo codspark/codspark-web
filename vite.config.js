bb761efac544ff00310d7dce8486b9a9af839a91
javed-ak <javedakhtarsofficial@gmail.com>
2025-08-14 00:57:45 +0530

    fu

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/codspark-web',
+  base: "codspark-web",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
