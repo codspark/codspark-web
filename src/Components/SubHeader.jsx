5274f5f90dba524d461b5beea5a4e3c84a4e6d6f
javed-ak <javedakhtarsofficial@gmail.com>
2025-08-14 10:58:48 +0530

    subheader updated

--- src/Components/SubHeader.jsx
+++ src/Components/SubHeader.jsx
@@ -3,12 +3,12 @@ export default function SubHeader() {
         <div>
             <div className="hidden lg:block bg-primary text-white text-xs p-1 overflow-hidden whitespace-nowrap">
                 <div className="animate-marquee inline-block">
-                    We're currently working on our main domain and will be live soon.<span className="text-red-500">*</span>
+                    Join Our Exciting Internship Journey.<span className="text-red-500">*</span>
                 </div>
             </div>
             <div className="lg:hidden bg-primary text-white text-xs p-1 overflow-hidden whitespace-nowrap">
                 <div className="animate-p_marquee inline-block">
-                    We're currently working on our main domain and will be live soon.<span className="text-red-500">*</span>
+                    Join Our Exciting Internship Journey.<span className="text-red-500">*</span>
                 </div>
             </div>
         </div>
