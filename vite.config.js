54ac0eef19ee12d98ad924b8e9b336ea9b081479
javed-ak <javedakhtarsofficial@gmail.com>
2025-08-14 17:50:42 +0530

    file updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// Detect build mode (GitHub Pages or custom domain)
+const isGitHubPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: isGitHubPages ? "/codspark-web/" : "/", //auto switch
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
